6cfa93bd06b1b800a9ee15c75dbb0975e30627a1
alexizvely <alexizvely@gmail.com>
2015-09-29 16:12:31 +0300

    Homeworks for Telerik Academy 2015 - adjusted folder order and better naming of files/folders.

---
+++ Module 1/CSharp-Part-1/Intro-Programming/07PrintFirstAndLastName/PrintFirstAndLastName.cs
@@ -0,0 +1,17 @@
+﻿///Problem 7. Print First and Last Name
+///Create console application that prints your first and last name, each at a separate line.
+
+using System;
+
+class PrintFirstAndLastName
+{
+    static void Main()
+    {
+
+        Console.WriteLine("R2 D2");
+            ///the following line is to keep the console open. You can press any key to exit the console.
+        Console.ReadKey();
+
+    }
+}
+
---
+++ Module 1/CSharp-Part-1/Intro-Programming/08SquareRoot/SquareRoot.cs
@@ -0,0 +1,18 @@
+﻿///Problem 8. Square Root
+///Create a console application that calculates and prints the square root of the number 12345.
+///Find in Internet “how to calculate square root in C#”.
+
+using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+
+        Console.WriteLine(Math.Sqrt(12345));
+            ///the following line is to keep the console open. You can press any key to exit the console.
+        Console.ReadKey();
+
+    }
+}
+
